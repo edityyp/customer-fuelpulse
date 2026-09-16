334da319e224001bc9550d74a2cbec7a21d65414
edityyp <editeasebusiness@gmail.com>
2026-09-16 11:19:19 +0530

    Add production first-time Owner setup removal script

---
+++ dist/hide-first-owner-setup.js
@@ -0,0 +1,1 @@
+const removeFirstOwnerSetup=()=>document.getElementById('setup')?.remove();new MutationObserver(removeFirstOwnerSetup).observe(document.body,{childList:true,subtree:true});removeFirstOwnerSetup();
